9e349208e3f61544b7aa46ec372f1398b0de71a0
j_domoslai <j_domoslai@fanshaweonline.ca>
2020-10-20 23:31:54 -0400

    Started building the background and parallax scrolling

---
+++ Assets/Scripts/BackgroundElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BackgroundElement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform neighbour;
+
+    public void Move()
+    {
+        transform.Translate(Vector2.left * speed * Time.smoothDeltaTime);
+    }
+
+    public void SnapToNeighbour()
+    {
+        transform.position = new Vector2(neighbour.position.x + 20, transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Reset")
+        {
+            SnapToNeighbour();
+        }
+    }
+}
