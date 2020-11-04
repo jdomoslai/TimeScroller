5cac295d70622579a9b357bbb236ee607c864417
j_domoslai <j_domoslai@fanshaweonline.ca>
2020-11-03 20:59:57 -0500

    Spawn background, platform and obstacle objects

---
+++ Assets/Scripts/MovableObstacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MovableObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private Obstacle[] obstacles;
+
+    [SerializeField]
+    private Obstacle[] platforms;
+
+    private List<Obstacle> movingObstacles;
+
+    private Obstacle currentObstacle = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingObstacles = new List<Obstacle>();
+        currentObstacle = Instantiate(platforms[3], new Vector3(20, 0.4f, 0), Quaternion.identity);
+        movingObstacles.Add(currentObstacle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingObstacles.Count > 0)
+        {
+            foreach (Obstacle mo in movingObstacles)
+            {
+                mo.Move();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private bool stationary;
+
+    [SerializeField]
+    private bool platform;
+
+    // Moves GameObjects to the left
+    public void Move()
+    {
+        transform.Translate(Vector2.left * speed * Time.smoothDeltaTime);
+    }
+}
