b7e5bc2c2985bd4ad77409e123bd927fb509afa1
j_domoslai <j_domoslai@fanshaweonline.ca>
2020-11-04 19:01:24 -0500

    Added timed release of platforms and obstacles

--- Assets/Scripts/MovableObstacle.cs
+++ Assets/Scripts/MovableObstacle.cs
@@ -11,27 +11,109 @@ public class MovableObstacle : MonoBehaviour
     [SerializeField]
     private Obstacle[] platforms;
 
+    [SerializeField]
+    private float spawnSecondsPlatforms;
+
+    [SerializeField]
+    private float deleteSecondsPlatforms;
+
+    [SerializeField]
+    private float spawnSecondsObstacles;
+
+    [SerializeField]
+    private float deleteSecondsObstacles;
+
     private List<Obstacle> movingObstacles;
 
-    private Obstacle currentObstacle = null;
+    private List<Obstacle> movingPlatforms;
+
+    private float spawnTimePlatforms;
+
+    private float deleteTimePlatforms;
+
+    private float spawnTimeObstacles;
+
+    private float deleteTimeObstacles;
 
     // Start is called before the first frame update
     void Start()
     {
         movingObstacles = new List<Obstacle>();
-        currentObstacle = Instantiate(platforms[3], new Vector3(20, 0.4f, 0), Quaternion.identity);
-        movingObstacles.Add(currentObstacle);
+
+        movingPlatforms = new List<Obstacle>();
+
+        spawnTimePlatforms = Time.time + spawnSecondsPlatforms;
+
+        deleteTimePlatforms = Time.time + deleteSecondsPlatforms;
+
+        spawnTimeObstacles = Time.time + spawnSecondsObstacles;
+
+        deleteTimeObstacles = Time.time + deleteSecondsObstacles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movingObstacles.Count > 0)
+        ConstantMovement(movingObstacles);
+
+        ConstantMovement(movingPlatforms);
+
+        Spawn(movingPlatforms, platforms, ref spawnTimePlatforms, spawnSecondsPlatforms);
+
+        Delete(movingPlatforms, ref deleteTimePlatforms, deleteSecondsPlatforms);
+
+        Spawn(movingObstacles, obstacles, ref spawnTimeObstacles, spawnSecondsObstacles);
+
+        Delete(movingObstacles, ref deleteTimeObstacles, deleteSecondsObstacles);
+    }
+
+    // Moves Obstacles if they exist
+    private void ConstantMovement(List<Obstacle> obstacles)
+    {
+        if (obstacles.Count > 0)
         {
-            foreach (Obstacle mo in movingObstacles)
+            foreach (Obstacle o in obstacles)
             {
-                mo.Move();
+                o.Move();
             }
         }
+    }
+
+    // Spawns random moving Obstacles after a certain time in seconds
+    private void Spawn(List<Obstacle> obstacleList, Obstacle[] obstacles, ref float spawnTime, float spawnSeconds)
+    {
+        if (Time.time > spawnTime)
+        {
+            int rand = Random.Range(0, obstacles.Length);
+            Obstacle obstacle = obstacles[rand];
+
+            obstacle = Instantiate(obstacle, new Vector3(20, RandomPosition(), 0), Quaternion.identity);
+            obstacleList.Add(obstacle);
+
+            spawnTime += spawnSeconds;
+        }
+    }
+
+    // Deletes moving Obstacles after a certain time in seconds
+    private void Delete(List<Obstacle> obstacleList, ref float deleteTime, float deleteSeconds)
+    {
+        if (Time.time > deleteTime)
+        {
+            Obstacle platformRemove = obstacleList.FirstOrDefault();
+
+            if (platformRemove)
+            {
+                obstacleList.Remove(platformRemove);
+                Destroy(GameObject.Find(platformRemove.name));
+
+                deleteTime += deleteSeconds;
+            }
+        }
+    }
+
+    // Returns a random position within the camera's Y range
+    private float RandomPosition()
+    {
+        return Random.Range(-3.0f, 5.0f);
     }
 }
