fbb23409e46e43b264fd462d8e46d314c0aa98ed
Josh LI <tree1112666@163.com>
2020-12-07 21:40:06 -0500

    Added AI

---
+++ Assets/Scripts/Ai.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ai : MonoBehaviour
+{
+    public float movementSpeed;
+    private Rigidbody2D characterRBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //facingRight = true;
+        characterRBody = GetComponent<Rigidbody2D>();
+    }
+
+
+}
